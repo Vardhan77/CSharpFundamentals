87ec5a18a64427306dd4c1dd3d4793692af2d5f0
Vardhan Honmane <vardhan834@gmail.com>
2020-01-21 09:55:18 +0530

    Add files via upload

---
+++ DataTypes/DataTypes/DataTypes/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace DataTypes
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /*int x;
+            int y;
+            x = 9;
+            y = x + 3;
+            Console.WriteLine(y);
+            Console.ReadLine();
+            */
+            Console.WriteLine("Whats your Name?");
+            Console.Write("Enter the first name");
+            string firstName = Console.ReadLine();
+            Console.Write("Enter the Last Name");
+            string lastName = Console.ReadLine();
+            Console.WriteLine("your NAME is: " + firstName +" " +  lastName);
+            Console.ReadLine();
+
+
+        }
+    }
+}
