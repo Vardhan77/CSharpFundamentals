ff66ec218b467a84bb15eedade4764849674c481
Vardhan Honmane <vardhan834@gmail.com>
2020-01-21 09:46:47 +0530

    Add files via upload

---
+++ Arrays/Arrays/Arrays/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Arrays
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            string zig = "Nobody cares how much you know," +
+                " until they know how much you care.";
+            char[] charArray = zig.ToCharArray();
+            Array.Reverse(charArray);
+
+            foreach(char zigChar in charArray)
+            {
+                Console.Write(zigChar);
+            }
+            Console.ReadLine();
+
+        }
+    }
+}
